3cee24b513ef1372e4d1d09dc664736f2027a59e
Moskas <minemoskas@gmail.com>
2026-03-04 23:45:26 +0100

    feat: dynamic overlay creation

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,11 +1,7 @@
-final: prev: {
-  reader = prev.callPackage ../pkgs/reader.nix { };
-  mpdnotify = prev.callPackage ../pkgs/mpdnotify.nix { };
-  whdl = prev.callPackage ../pkgs/whdl.nix { };
-  epy = prev.callPackage ../pkgs/epy.nix { };
-  osu-bin = prev.callPackage ../pkgs/osu-bin.nix { };
-  tickrs = prev.callPackage ../pkgs/tickrs.nix { };
-  etterna = prev.callPackage ../pkgs/etterna/etterna.nix { };
-  anifetch = prev.callPackage ../pkgs/anifetch.nix { };
-  hakurei-reimu = prev.callPackage ../pkgs/cursors/reimu.nix { };
-}
+final: prev:
+let
+  # Import and evaluate the pkgs/default.nix file relative to overlay location
+  customPkgs = import ../pkgs { inherit (prev) callPackage lib stdenv; };
+in
+# Merge packages into final set (prev takes precedence on conflicts)
+customPkgs // final
